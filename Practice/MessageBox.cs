f746947ee5956a0afe1d7ee1ea0e3e17a7e27da3
Larysa Hradyska <l.v.hradyska@gmail.com>
2023-03-18 12:50:23 +0200

    adding solution

---
+++ Practice/MessageBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class MessageBox
+    {
+        public event EventHandler<State> OnWinClosed;
+        public async void Open()
+        {
+            Console.WriteLine("Window is opened.");
+            await Task.Delay(300);
+            Console.WriteLine("Window is closed.");
+            OnWinClosed?.Invoke(this, StateOnClosed());
+        }
+        public enum State
+        {
+            Ok,
+            Cancel
+        }
+        
+        public static State StateOnClosed()
+        {
+            Random rand = new Random();
+            int i = rand.Next(0, 100);
+            if (i<50)
+            {
+                return State.Ok;
+            }
+            else
+            {
+                return State.Cancel;
+            }
+        }
+    }
+}
